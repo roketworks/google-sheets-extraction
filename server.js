91a3a6253bce2cd140e4f5935da26955a1e63b11
roketworks <roketworks@users.noreply.github.com>
2015-08-14 14:43:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 var http = require('http')
 
 //Required for Azure
-var port = process.env.PORT || 1337; 
+var port = process.env.PORT || 1337;
 var GoogleSpreadsheet = require("google-spreadsheet");
- 
+
 // spreadsheet key is the long id in the sheets URL 
-var workbook = new GoogleSpreadsheet('1EJuCSP2ki9WrHM_NBIoZomft-FjXljar-KZsZzWWRL0');
-var totalSheet; 
-var moneySheet; 
+var workbook = new GoogleSpreadsheet('INSERT SHEET ID HERE');
+var totalSheet;
+var moneySheet;
 
-http.createServer(function (req, res){
-	// restrict to domain
-	res.setHeader("Access-Control-Allow-Origin", "*"); 
+http.createServer(function(req, res) {
+    // restrict to domain
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-	res.writeHead(200, {'Content-Type': 'application/json'});
-	
-	workbook.getInfo(function(err, info){
-		if (err) return console.log(err); 
-    	console.log(info);
-    	sheet = info.worksheets[2];
-  		sheet.getRows(function(error, rows){
-  			console.log(rows);
-	  	
-	  		var response = [];
-			
-          // Output the response from Google Sheets into my own desired format
-	  		for (i = 0; i < rows.length; i++) {
-	  			if (rows[i].person != "Total:") {
-		  			response.push({
-		  				"label": rows[i].label, 
-		  				"val2": rows[i].val1, 
-		  				"val2": rows[i].val2		
-		  			});
-	  			}
-	  		}	
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
 
-  			res.end(JSON.stringify(response));
-	  	});
-	});
+    workbook.getInfo(function(err, info) {
+        if (err) {
+            return console.log(err);
+        }
+        sheet = info.worksheets[2];
+        sheet.getRows(function(error, rows) {
+
+            var response = [];
+
+            // Output the response from Google Sheets into my own desired format
+            for (i = 0; i < rows.length; i++) {
+                if (rows[i].person != "Total:") {
+                    response.push({
+                        "label": rows[i].label,
+                        "val2": rows[i].val1,
+                        "val2": rows[i].val2
+                    });
+                }
+            }
+
+            res.end(JSON.stringify(response));
+        });
+    });
 }).listen(port);
